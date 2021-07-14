f6e4d5c075da22f048e13c8ec97e8e0bcd22871f
madhuanandam <madhu.anandam@gmail.com>
2021-07-14 16:14:02 +0100

    Php email file

--- subscribe.php
+++ subscribe.php
@@ -1,9 +1,5 @@
 
 <?php
-$servername = "localhost";
-$username = "u617208335_stunetdata";
-$password = "Stunet@1509";
-$dbname = "u617208335_registereddata";
 
 $firstname=$_POST["first-name"];
 $lastname=$_POST["last-name"];
@@ -11,23 +7,21 @@ $email=$_POST["your_email"];
 $phone=$_POST["phone"];
 $tech=$_POST["tech"];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-/*$sql = "INSERT INTO userdata (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";*/
-$sql = 'INSERT INTO userdata (firstname,lastname,email,phone,technology)
-VALUES ('$firstname','$lastname','$email','$phone','$tech);
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "welcome@poolup.4not5.com";
+    $to = "madhu.anandam@gmail.com,sowjanyachikka405@gmail.com,v.rakesh40@gmail.com";
+    $subject = "Thank you for Registering";
+    $message = "Hi Team \n".$firstname. " " .$lastname." with 
+Email: ".$email.",
+phone: ".$phone.",
+Techonology: ".$tech." 
+has registered with you for proxy";
+    $headers = "From:" . $from;
+	
+    if(mail($to,$subject,$message, $headers)) {
+		echo "The email message was sent.";
+    } else {
+    	echo "The email message was not sent.";
+    }
 ?>
