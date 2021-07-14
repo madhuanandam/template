47d5cb5c12d473cbb4862f712189f1307187795d
madhuanandam <madhu.anandam@gmail.com>
2021-07-14 19:59:31 +0100

    Email Functionality to send  simultaneous emails
    Email Functionality to send  simultaneous emails including user and 4not5

--- subscribe3.php
+++ subscribe3.php
@@ -19,47 +19,101 @@ if ($conn->connect_error) {
 
 /*$sql = "INSERT INTO userdata (firstname, lastname, email)
 VALUES ('John', 'Doe', 'john@example.com')";*/
-$sql = "INSERT INTO userdata (firstname,lastname,email,phone,technology)
+$sql = "INSERT INTO userdata_test (firstname,lastname,email,phone,technology)
 VALUES ('".$firstname."','".$lastname."','".$email."','".$phone."','".$tech."')";
 
-if ($conn->query($sql) === TRUE) {
+$VALUE = $conn->query($sql);
+
+
+if ($VALUE) {
 	ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "Yayyy@poolup.4not5.com";
-    $to = "sowjanyachikka405@gmail.com";
+    $from = "Welcome Champ@poolup.4not5.com";
+    $to = "madhu.anandam@gmail.com";
     $subject = "Thank you for Registering";
     $message = ' 
     <html> 
     <head> 
-        <title>Yayyy! We got a new user named '.$firstname.' '.lastname.'</title> 
+        <title></title> 
     </head> 
     <body> 
-        <h1>Yayyy! We got a new user named '.$firstname.' '.$lastname.'</h1> 
+        <h1>Yayyy! We are delighted to welcome to on board!</h1> 
         <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
             <tr> 
-                <th>Email:</th><td>CodexWorld</td> 
+                <th>Email:</th><td>'.$email.'</td> 
             </tr> 
             <tr style="background-color: #e0e0e0;"> 
-                <th>Phone:</th><td>contact@codexworld.com</td> 
+                <th>Phone:</th><td>'.$phone.'</td> 
             </tr> 
             <tr> 
-                <th>Technology:</th><td><a href="http://www.codexworld.com">www.codexworld.com</a></td> 
+                <th>Technology:</th><td>'.$tech.'</td> 
             </tr> 
         </table> 
     </body> 
     </html>';
     
-$headers = "MIME-Version: 1.0" . "\r\n"; 
-$headers = "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+	$headers = "MIME-Version: 1.0" . "\r\n"; 
+	$headers = "Content-type:text/html;charset=UTF-8" . "\r\n"; 
 	
-$headers .= 'From: '.$from . "\r\n"; 
-    if(mail($to,$subject,$message, $headers)) { 	
+	$headers .= 'From: '.$from . "\r\n"; 
+
+
+
+    if(mail($to,$subject,$message, $headers)) { 
+		
+		
+	 //header('Location: success.html');
+
+    } else {
+    	echo "The email message was not sent.";
+    }
+} 
+
+if($VALUE){
+	ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "Yayyy@poolup.4not5.com";
+    $to = "madhu.anandam@gmail.com";
+    $subject = 'We got a new user named '.$firstname.' '.$lastname.'';
+    $message = ' 
+    <html> 
+    <head> 
+        <title></title> 
+    </head> 
+    <body> 
+        <h1>Yayyy! We got a new user named '.$firstname.' '.$lastname.'</h1> 
+        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
+            <tr> 
+                <th>Email:</th><td>'.$email.'</td> 
+            </tr> 
+            <tr style="background-color: #e0e0e0;"> 
+                <th>Phone:</th><td>'.$phone.'</td> 
+            </tr> 
+            <tr> 
+                <th>Technology:</th><td>'.$tech.'</td> 
+            </tr> 
+        </table> 
+    </body> 
+    </html>';
+    
+	$headers = "MIME-Version: 1.0" . "\r\n"; 
+	$headers = "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+	
+	$headers .= 'From: '.$from . "\r\n"; 
+
+
+
+    if(mail($to,$subject,$message, $headers)) { 
+		
+		
 	 header('Location: success.html');
 
     } else {
     	echo "The email message was not sent.";
     }
-} else {
+}
+
+else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
